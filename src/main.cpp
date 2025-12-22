d8b5ad4f78ee1357ce9c00f10bc7ac073aa54792
Aidan Andrew <aidan@graduate.utm.my>
2025-12-23 00:46:38 +0800

    Add movement
    Using endless loop (while(true))
    using windows(clear) for clean terminal
    used coordinate-based movement

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 #include <string>
 using namespace std;
 
+#ifdef _WIN32
+#define CLEAR "cls"
+#else
+#define CLEAR "clear"
+#endif
+
 int main() {
     vector<string> map= {
         "####################",
@@ -15,16 +21,39 @@ int main() {
     //player position
     int playerX = 1;
     int playerY = 1;
+    
+    while (true) {
+        system(CLEAR);
 
-    for (int y = 0; y < map.size(); y++) {
-        for (int x = 0; x < map[y].size(); x++) {
-            if (x == playerX && y == playerY) {
-                cout << '@'; // Draw player
-            } else {
-                cout << map[y][x]; // Draw map
-            }
-        }
         cout << endl;
+        for (int y = 0; y < map.size(); y++) {
+            for (int x = 0; x < map[y].size(); x++) {
+                if (x == playerX && y == playerY) {
+                    cout << '@'; // Draw player
+                } else {
+                    cout << map[y][x]; // Draw map
+                }
+            }
+            cout << endl;
+        }
+
+        cout << "\nMove(wasd), quit(q): ";
+        char input;
+        cin >> input;
+
+        int newPlayerX = playerX;
+        int newPlayerY = playerY;
+
+        if (input == 'w') newPlayerY--;
+        else if (input == 's') newPlayerY++;
+        else if (input == 'a') newPlayerX--;
+        else if (input == 'd') newPlayerX++;
+        else if (input == 'q') break;
+        // Check for collisions with walls
+        if (map[newPlayerY][newPlayerX] != '#') {
+            playerX = newPlayerX;
+            playerY = newPlayerY;
+        }
     }
 
     return 0;
