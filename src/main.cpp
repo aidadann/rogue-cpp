ea3470f4403278c32585123e91d2814759380fc7
Aidan Andrew <aidan@graduate.utm.my>
2025-12-22 20:44:11 +0800

    Render player in the map
    Use overlay system (nested loop).
    prevent overwriting the map data.
    separation of data vs entities

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,19 @@ int main() {
         "####################"
     };
 
-    for (const string& row : map) {
-        cout << row << endl;
+    //player position
+    int playerX = 1;
+    int playerY = 1;
+
+    for (int y = 0; y < map.size(); y++) {
+        for (int x = 0; x < map[y].size(); x++) {
+            if (x == playerX && y == playerY) {
+                cout << '@'; // Draw player
+            } else {
+                cout << map[y][x]; // Draw map
+            }
+        }
+        cout << endl;
     }
 
     return 0;
